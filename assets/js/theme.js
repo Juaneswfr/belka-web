9d5227699db097144bf6ffcb0c7245f9b3da5ad7
Juanes <>
2022-04-25 00:27:04 -0500

    init

---
+++ assets/js/theme.js
@@ -0,0 +1,91 @@
+gsap.registerPlugin(ScrollTrigger);
+
+
+gsap.utils.toArray(".comparisonSection").forEach(section => {
+	let tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: section,
+				start: "center center",
+        // makes the height of the scrolling (while pinning) match the width, thus the speed remains constant (vertical/horizontal)
+				end: () => "+=" + section.offsetWidth, 
+				scrub: true,
+				pin: true,
+        anticipatePin: 1
+			},
+			defaults: {ease: "none"}
+		});
+	// animate the container one way...
+	tl.fromTo(section.querySelector(".afterImage"), { xPercent: 100, x: 0}, {xPercent: 0})
+	  // ...and the image the opposite way (at the same time)
+	  .fromTo(section.querySelector(".afterImage img"), {xPercent: -100, x: 0}, {xPercent: 0}, 0);
+});
+
+
+gsap.utils.toArray("#secondary-section").forEach(section => {
+	let tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: section,
+				start: "center center",
+                // makes the height of the scrolling (while pinning) match the width, thus the speed remains constant (vertical/horizontal)
+				end: () => "+=" + section.offsetHeight, 
+				scrub: true,
+		        pin: true,
+                anticipatePin: 1,
+                toggleClass: "active"
+			},
+			defaults: {ease: "none"}
+		});
+
+    tl.to( ".main-rocket" , {duration: 1 , y:-300  ,delay:0.4} ,"someLabel" )
+    tl.set(".rocket" , {className:"+=show-map"});
+
+});
+
+let tlMainLogo = gsap.timeline();
+let tlHeader = gsap.timeline();
+
+// ANIMACIÓN LOGO
+
+tlMainLogo.to(".main-logo, .elementor-heading-title" , { 
+    y: -300,
+    ease : 'none',
+    opacity:0,
+    duration:3
+});
+
+// TRIGER ANIMACION LOGO SECCION 1
+
+ScrollTrigger.create({
+    trigger: ".main-logo",
+    animation: tlMainLogo,
+    markers: true,
+    start: "center center",
+    end: "bottom center",
+    scrub: 1,
+});
+
+
+
+// ANIMACIÓN HEADER
+tlHeader.to("#main-header" , { 
+    y: 0,
+    ease : 'none',
+    opacity:1,
+    duration:0.6
+});
+
+
+// TRIGGER ANIMACIÓN HEADER
+
+ScrollTrigger.create({
+    trigger: ".section-test",
+    animation: tlHeader,
+    markers: true,
+    start: "center center",
+    end: "bottom center",
+    toggleActions : "play none none reverse ",
+});
+
+
+
+
---
+++ index.php
@@ -0,0 +1,131 @@
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+<!-- meta tags -->
+<meta charset="utf-8">
+<meta name="keywords" content="páginas web visibles, modernas, limpias y responsivas, responsive" />
+<meta name="description" content="Diseñamos páginas web visibles, modernas, limpias y responsivas con todos los estándares de calidad. Desarrollamos aplicaciones Android y Apple Store a tu medida y necesidad." />
+<meta name="author" content="www.bluewebfactory.com" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Title -->
+<title>BELKA COMPANY</title>
+
+<!-- Favicon Icon -->
+<link rel="shortcut icon" href="assets/images/favicon.png" />
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
+
+<!-- inject css start -->
+
+<link href="assets/css/normalize.css" rel="stylesheet" />
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+
+<!--== theme.css -->
+<link href="assets/css/theme.css" rel="stylesheet" />
+
+
+<!-- inject css end -->
+
+</head>
+
+<body class="custom-background">
+
+
+<div class="stars"></div>
+<div class="twinkling"></div>
+
+
+ <header id="main-header">
+    <ul>
+        <li>TEST</li>
+        <li>TEST</li>
+        <li>TEST</li>
+        <li>TEST</li>
+    </ul>
+ </header>
+
+ 
+ <main>
+
+    <section id="primary-section" class="animate__animated  animate__fadeIn">
+        <div class="container-image main-logo">
+            <img width="443" height="395" src="assets/image/logo-belka-company-2.png" alt="" loading="lazy">
+        </div>        
+        <h1 class="heading-title elementor-heading-title">¿Estas listo para despegar?</h1>
+    </section>
+
+    
+    <section id="secondary-section" class="">
+        <div class="container-earth">
+            <div class="clouds"></div>
+            <div class="main-rocket ascending">
+                <div class="rocket"></div>
+            </div>
+            <div class="earth">
+                <div class="circle"></div>                
+            </div>  
+            <div class="atmosfera"></div>
+        </div>        
+    </section>
+
+
+
+
+    <h1 class="header-section">Scroll to see the before/after</h1>
+
+    <section class="comparisonSection">
+    <div class="comparisonImage beforeImage">
+        <img src="https://assets.codepen.io/16327/before.jpg" alt="before">
+    </div>
+    <div class="comparisonImage afterImage">
+        <img src="https://assets.codepen.io/16327/after.jpg" alt="after">
+    </div>
+    </section>
+
+    <h1 class="header-section">What did you think?</h1>
+
+
+    <section id="third-section" class="">
+        <div class="container-rocket">
+            <h2>¿Cuál es tu <br> <span> objetivo?</span></h2>
+            <div class="container-image">
+                <img src="assets/image/rocket_1.svg" width="400px" alt="">
+            </div>
+        </div>     
+        <div class="container-planets">
+            <a href="">
+                <h3>Vender</h3>
+                <img src="assets/image/planet_vender.svg" alt="">
+            </a>
+        </div>   
+    </section>
+
+
+    <div class="section-test">
+
+    </div>
+
+
+</main>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.3.4/gsap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.3.4/ScrollTrigger.min.js"></script>
+<!-- inject js start -->
+
+<script src="assets/js/theme.js"></script>
+
+<!-- inject js end -->
+
+
+
+
+</body>
+
+</html>
